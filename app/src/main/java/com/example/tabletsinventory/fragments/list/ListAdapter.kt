c8d386eb59b31d4fa9f9fff4923f32f2ea76a3a4
ahs1n <ahsanpervez74@gmail.com>
2021-04-12 11:18:48 +0500

    *Commit

--- app/src/main/java/com/example/tabletsinventory/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/tabletsinventory/fragments/list/ListAdapter.kt
@@ -30,13 +30,9 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = tabletList[position]
-        holder.itemView.id_txt.text = currentItem.id.toString()
         holder.itemView.tagNum_txt.text = currentItem.tagNum
         holder.itemView.location_txt.text = currentItem.location
         holder.itemView.custodian_txt.text = currentItem.custodian
-        holder.itemView.accessoriesSend_txt.text = currentItem.accessoriesSend
-        holder.itemView.accessoriesRec_txt.text = currentItem.accessoriesRec
-        holder.itemView.issueDate_txt.text = currentItem.issueDate
         holder.itemView.projectName_txt.text = currentItem.projectName.convertStringToUpperCase()
 
         holder.itemView.rowLayout.setOnClickListener {
